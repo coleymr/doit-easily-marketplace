0dfb57075b804955c4e3cdfacc1253ca6055463f
Jordan Speers <jordan.speers@cirata.com>
2025-03-13 14:40:33 +0000

    IT-19291: Move Terraform State file to cloud

--- docs/terraform/app_deploy/backend.tf
+++ docs/terraform/app_deploy/backend.tf
@@ -1,6 +1,8 @@
+# Setup remote terraform state file in Google Cloud Storage
+# State locking is enabled by default when using Google Cloud Storage as a backend.
 terraform {
-  # backend "gcs" {
-  # bucket = "doit-test-davec-public-terraform"
-
-  # }
+  backend "gcs" {
+    bucket = "tfstate-wandisco-public-384719"
+    prefix = "doit-marketplace"
+  }
 }

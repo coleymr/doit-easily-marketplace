4b7e82a5b16992e0e41947a1d32ac62c55c3166b
Jordan Speers <jordan.speers@cirata.com>
2025-03-13 14:55:21 +0000

    IT-19291: Re-organize file structure

--- docs/terraform/setup/example.tfvars
+++
@@ -1,3 +0,0 @@
-folder_id = "numerical ID of the folder"
-project_id = "your-marketplace-project-id"
-billing_id = "billing id for your project"
--- docs/terraform/setup/project.tf
+++
@@ -1,8 +0,0 @@
-
-resource "google_project" "isv-public" {
-  name       = var.project_id
-  project_id = var.project_id
-  folder_id = var.folder_id
-  billing_account = var.billing_id
-}
-
